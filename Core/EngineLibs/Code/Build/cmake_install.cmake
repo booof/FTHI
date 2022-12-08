5d10aad7f40b1bab6bcc82f9f033aeda8051fdda
Elliot Orr <elliot.orr02@gmail.com>
2022-12-07 20:11:01 -0800

    Storing Build Files in Case They Are Deleted

--- Core/EngineLibs/Code/Build/cmake_install.cmake
+++ Core/EngineLibs/Code/Build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ellio/OneDrive/Documents/Visual Studio 2019 - Copy/projects/Game Engine/FTHI/Core/EngineLibs/Code
+# Install script for directory: C:/Users/ellio/source/repos/FTHI/Core/EngineLibs/Code
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,13 +34,13 @@ endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/ellio/OneDrive/Documents/Visual Studio 2019 - Copy/projects/Game Engine/FTHI/Core/EngineLibs/Code/Build/Debug/enginecode.lib")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/ellio/source/repos/FTHI/Core/EngineLibs/Code/Build/Debug/enginecode.lib")
   elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/ellio/OneDrive/Documents/Visual Studio 2019 - Copy/projects/Game Engine/FTHI/Core/EngineLibs/Code/Build/Release/enginecode.lib")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/ellio/source/repos/FTHI/Core/EngineLibs/Code/Build/Release/enginecode.lib")
   elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/ellio/OneDrive/Documents/Visual Studio 2019 - Copy/projects/Game Engine/FTHI/Core/EngineLibs/Code/Build/MinSizeRel/enginecode.lib")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/ellio/source/repos/FTHI/Core/EngineLibs/Code/Build/MinSizeRel/enginecode.lib")
   elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/ellio/OneDrive/Documents/Visual Studio 2019 - Copy/projects/Game Engine/FTHI/Core/EngineLibs/Code/Build/RelWithDebInfo/enginecode.lib")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Users/ellio/source/repos/FTHI/Core/EngineLibs/Code/Build/RelWithDebInfo/enginecode.lib")
   endif()
 endif()
 
@@ -52,5 +52,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ellio/OneDrive/Documents/Visual Studio 2019 - Copy/projects/Game Engine/FTHI/Core/EngineLibs/Code/Build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ellio/source/repos/FTHI/Core/EngineLibs/Code/Build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
